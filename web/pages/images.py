7c63142197b4c6557327aa00ba14f8913320d47e
Nangong66 <88227552+NJkirito@users.noreply.github.com>
2025-09-22 05:08:54 +1000

    Update images.py

--- web/pages/images.py
+++ web/pages/images.py
@@ -1,9 +1,12 @@
+from pathlib import Path
 import streamlit as st
+
+BASE_DIR = Path(__file__).parent.parent  # web/
 
 st.title("WELCOME TO SWEETY!")
 
 st.title("My banner:")
-st.image('assets/banner.png')
+st.image(str(BASE_DIR / 'assets' / 'banner.png'))
 
 st.title("My poster:")
-st.image('assets/poster.png')
+st.image(str(BASE_DIR / 'assets' / 'poster.png'))
