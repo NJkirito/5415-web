25fa7df3aaf357b88350dc1aace9d93a14202483
Nangong66 <88227552+NJkirito@users.noreply.github.com>
2025-09-22 05:12:37 +1000

    Update videos.py

--- web/pages/videos.py
+++ web/pages/videos.py
@@ -1,13 +1,19 @@
 import streamlit as st
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent.parent  # web/
 
 st.title("WELCOME TO SWEETY!")
-
 st.title("My video:")
+
+
+video_path = BASE_DIR / "assets" / "sunset.MP4"
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    st.video("assets/sunset.MP4")
+    st.video(str(video_path))
 with col2:
-    st.video("assets/sunset.MP4")
+    st.video(str(video_path))
 with col3:
-    st.video("assets/sunset.MP4")
+    st.video(str(video_path))
