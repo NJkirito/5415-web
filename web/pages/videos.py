ee1e1378e1ba5c650f4fb102854c494004b65f23
Nangong66 <88227552+NJkirito@users.noreply.github.com>
2025-10-22 13:50:55 +1100

    Update videos.py

--- web/pages/videos.py
+++ web/pages/videos.py
@@ -1,15 +1,13 @@
 import streamlit as st
 from pathlib import Path
 
-# 获取 videos.py 所在目录
 BASE_DIR = Path(__file__).parent.parent  # web/
 
 st.title("WELCOME TO SWEETY!")
 st.title("My video:")
 
-# 视频文件路径
-video1_path = BASE_DIR / "assets" / "video1.MP4"
-video2_path = BASE_DIR / "assets" / "video2.MP4"
+video1_path = BASE_DIR / 'assets' / 'video1.mp4'
+video2_path = BASE_DIR / 'assets' / 'video2.mp4'
 
 col1, col2 = st.columns(2)
 with col1:
